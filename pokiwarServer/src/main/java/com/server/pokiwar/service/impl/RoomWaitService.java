a6b3a5864b208feedb93cca609919776be26e357
LAPTOP-QO0M5VLN\ASUS <vuhongthien>
2024-12-02 22:57:11 +0700

    u

--- pokiwarServer/src/main/java/com/server/pokiwar/service/impl/RoomWaitService.java
+++ pokiwarServer/src/main/java/com/server/pokiwar/service/impl/RoomWaitService.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class RoomWaitService {
@@ -66,11 +67,19 @@ public class RoomWaitService {
                 enemyPetRepository.findById(request.getIdEnemyPet()).orElse(null)
         );
 
+
+
         // Fetch count pass
         CompletableFuture<Integer> countPassFuture = CompletableFuture.supplyAsync(() ->
-                countPassRepository.countPassBy(request.getIdEnemyPet(), request.getIdUser())
-                        .map(CountPass::getCount)
-                        .orElse(0)
+                {
+                    try {
+                        return countPassRepository.countPassBy(enemyPetFuture.get().getIdPet(), request.getIdUser())
+                                .map(CountPass::getCount)
+                                .orElse(0);
+                    } catch (InterruptedException | ExecutionException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
         );
 
         // Resolve futures
@@ -108,6 +117,12 @@ public class RoomWaitService {
                         .map(entity -> mapper.map(entity, PetUserDto.class))
                         .toList()
         );
+        try {
+            Pet petEnemy = petRepository.findById(enemyPetFuture.get().getIdPet()).get();
+            userPlayerDto.getEnemyPet().setRequestPass(petEnemy.getConditionPass());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
 
         // Set additional data
         userPlayerDto.setListChooseCard(cardsFuture.join());
