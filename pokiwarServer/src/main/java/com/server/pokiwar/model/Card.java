036a25ad6c11248c940768c0b22b05fd63683690
LAPTOP-QO0M5VLN\ASUS <vuhongthien>
2024-12-19 23:57:15 +0700

    lk

--- pokiwarServer/src/main/java/com/server/pokiwar/model/Card.java
+++ pokiwarServer/src/main/java/com/server/pokiwar/model/Card.java
@@ -1,9 +1,7 @@
 package com.server.pokiwar.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
